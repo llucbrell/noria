ccdfd4263989329d50cd3f0c581959177176906a
Lucas C / llucbrell <llucbrell@tutanota.com>
2025-11-02 17:32:48 +0100

    added pwa functionality

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'noria-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activación del Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activado');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Borrando cache antiguo', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Interceptar peticiones
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Si está en cache, devolverlo
+        if (response) {
+          return response;
+        }
+        // Si no, hacer la petición
+        return fetch(event.request);
+      })
+  );
+});
+
+// Manejar clicks en notificaciones
+self.addEventListener('notificationclick', (event) => {
+  console.log('Service Worker: Click en notificación');
+  event.notification.close();
+  
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
